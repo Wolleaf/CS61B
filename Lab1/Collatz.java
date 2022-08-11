fe648779c80e7906352351f5a404d2897e4ce1d6
Wolleaf Lee <744901920@qq.com>
2022-08-11 19:30:43 +0800

    feat:Lab1,D.ProgrammingExercise

---
+++ Lab1/Collatz.java
@@ -0,0 +1,32 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Collatz {
+
+    /**
+     * If n is even, the next number is n/2. If n is odd, the next number is 3n + 1.
+     * @param n the number
+     * @return the next number of Collatz Sequence
+     */
+    public static int nextNumber(int n) {
+        if ((n & 1) == 0) return n / 2;
+        else return n * 3 + 1;
+    }
+
+    /**
+     * Type in a number and print the Collatz Sequence
+     * @param args none
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please type in the number:");
+        int n = in.nextInt();
+        System.out.println();
+        while (n != 1) {
+            System.out.print(n + " ");
+            n = nextNumber(n);
+        }
+        System.out.println(n);
+    }
+}
